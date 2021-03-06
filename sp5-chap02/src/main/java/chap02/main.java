3f9e38da0a20ff39289d4a3e9977025d31db5eb0
devbelly <devbelly@naver.com>
2021-03-06 13:22:37 +0900

    폴더 commit 테스트

--- sp5-chap02/src/main/java/chap02/main.java
+++ sp5-chap02/src/main/java/chap02/main.java
@@ -9,6 +9,7 @@ public class main {
 				new AnnotationConfigApplicationContext(AppContext.class);
 		
 		Greeter g = ctx.getBean("greeter",Greeter.class);
+		Greeter g2 = ctx.getBean("greeter",Greeter.class);
 		String msg = g.greet("스프링");
 		System.out.println(msg);
 		ctx.close();
