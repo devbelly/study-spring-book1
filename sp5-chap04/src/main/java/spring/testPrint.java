55e31f648354239dcf320b49bb375cfa29d16021
devbelly <devbelly@naver.com>
2021-03-09 14:57:49 +0900

    Nullable 어노테이션 사용

--- sp5-chap04/src/main/java/spring/testPrint.java
+++ sp5-chap04/src/main/java/spring/testPrint.java
@@ -1,10 +1,12 @@
 package spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 
 public class testPrint {
 	
 	@Autowired
+	@Nullable
 	private FastMemberDao memberDao;
 	
 	public void print() {
