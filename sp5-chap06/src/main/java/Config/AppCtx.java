d44e0a7bd90e8b714726d7fe6c855ebfbac3797d
devbelly <devbelly@naver.com>
2021-03-10 13:24:01 +0900

    AppCtx prototype scope 추가

--- sp5-chap06/src/main/java/Config/AppCtx.java
+++ sp5-chap06/src/main/java/Config/AppCtx.java
@@ -1,12 +1,14 @@
 package Config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import spring.*;
 @Component
 public class AppCtx {
 	
 	@Bean
+	@Scope("prototype")
 	public Client client() {
 		return new Client();
 	}
