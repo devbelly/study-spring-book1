0913f2b98659274b2bd23b77c36552fbe93b0093
devbelly <devbelly@naver.com>
2021-04-02 13:19:35 +0900

    InitBinder을 통한 컨트롤러 범위 Validator

--- sp5-chap12/src/main/java/controller/RegisterController.java
+++ sp5-chap12/src/main/java/controller/RegisterController.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,6 +57,11 @@ public class RegisterController {
 			errors.rejectValue("email", "duplicate");
 			return "register/step2";
 		}
+	}
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new RegisterRequestValidator());
 	}
 
 }
